154059d012f864d5cf99bea248107aa783aeb4e4
neoadventist <git@spencersims.com>
2013-08-18 23:05:02 -0700

    Added random video background

--- assets/controller/main.js
+++ assets/controller/main.js
@@ -1,3 +1,14 @@
 app.controller('main', function ($scope, $timeout,$http,$filter) {
+	$scope.randomVid = function (){
+		videos =[	"http://www.convertro.com/sites/all/themes/custom/convertro/videos/crossing.webm",
+					"http://www.convertro.com/sites/all/themes/custom/convertro/videos/rainydayNYC.webm"
+				];
+		num = videos.length; 
+		vidId= Math.floor((Math.random()*num)+0);
+		return videos[vidId];
+	
+	}
+	console.log($scope.randomVid());
+	$scope.randomVidURL=$scope.randomVid();
 	$scope.start = "Welcome, My Name is Spencer Sims";
 });
