2df2caf31cd91b69957752eba8bac25bf6589678
neoadventist <git@spencersims.com>
2013-08-20 16:48:57 -0700

    Added expanding menu directive

--- assets/directives/main.js
+++ assets/directives/main.js
@@ -4,8 +4,21 @@ app.directive('roffset', function () {
 		restrict: 'A',
 		link: function (scope, elem, attrs) {	
 			var offset= "offset"+Math.floor((Math.random()*6)+0);
-			attrs.$set('class',offset+' span3 lguyFont menu');
-			console.log("Recognized the random offset directive usage");
+			attrs.$set('class',offset+' '+attrs.class);
+		}
+	}
+});
+app.directive('expand', function () {
+	//retruns a random offset for the menu
+	return {
+		restrict: 'A',
+		link: function (scope, elem, attrs) {
+			elem.on('click',function(){
+				var oldclass=attrs.class;
+				var newclass=oldclass.replace("span3","span6");
+				attrs.$set('class',newclass);
+			});
+			
 		}
 	}
 });
