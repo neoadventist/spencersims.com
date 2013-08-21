07e9eb6944fdcdf9505ee799a3715cc628f4c8da
neoadventist <git@spencersims.com>
2013-08-20 19:21:21 -0700

    fixed backround video, added more info about me

--- assets/controller/main.js
+++ assets/controller/main.js
@@ -11,19 +11,20 @@ app.controller('main', function ($scope, $timeout,$http,$filter) {
 	}
 	console.log($scope.randomVid());
 	$scope.randomVidURL=$scope.randomVid();
-	
+	$scope.randomFontFcn = function(){
+		fonts = ["Comfortaa","Luckiest+Guy","Righteous","Open+Sans"];
+		num = fonts.length; 
+		fontId= Math.floor((Math.random()*num)+0);
+		return fonts[fontId]
+	}
+	$scope.randomFont=$scope.randomFontFcn();
 	$scope.start = "Welcome, My Name is Spencer Sims";	
-	$scope.meDetail =false;
-	$scope.cvDetail =false;
-	$scope.codeDetail =false;
-	$scope.videoDetail =false;
-	$scope.projectDetail =false;
-	$scope.religionDetail =false;
-	$scope.me="My Name is Spencer Sims. I'm a Front End Enginner in Los Angeles, CA";
-	$scope.cv="You can view my CV HERE";
-	$scope.code="You can check out my github HERE";
-	$scope.videos="Subscribe to me on YouTube!";
+
+	$scope.me="<div>My Name is Spencer Sims.</div> <div>I'm a Front End Enginner in Los Angeles, CA</div><div><div>Find Me On:<ul>  <li><a href='http://spencersims.com/facebook'>Facebook</a></li>  <li><a href='http://spencersims.com/twitter'>Twitter</a></li>  <li><a href='http://spencersims.com/+'>G+</a></li>  <li><a href='http://spencersims.com/strava'>Strava</a></li>  <li><a href='http://spencersims.com/github'>GitHub</a></li>  <li><a href='http://spencersims.com/linkedin'>Linked-In</a></li>  <li><a href='http://spencersims.com/youtube'>Youtube</a></li></ul></div><br>";
+	$scope.cv="PDF | Linked-In";
+	$scope.code="You can check out my <a href='http://spencersims.com/github'>github HERE</a>";
+	$scope.videos="Subscribe to me on <a href='http://spencersims.com/youtube'>YouTube</a>!";
 	$scope.projects="You can see my recent projects HERE";
-	$scope.religion="Check out my magazine HERE";
+	$scope.religion="Check out my magazine <a href='http://eyesofreason.com'>HERE</a>";
 	
 });
--- assets/directives/main.js
+++ assets/directives/main.js
@@ -2,10 +2,23 @@ app.directive('roffset', function () {
 	//retruns a random offset for the menu
 	return {
 		restrict: 'A',
+		scope:{
+			subject:"@subject",
+			detail:"@detail"
+		},
 		link: function (scope, elem, attrs) {	
 			var offset= "offset"+Math.floor((Math.random()*6)+0);
 			attrs.$set('class',offset+' '+attrs.class);
-		}
+			
+			elem.on('click',function(){
+				var oldclass=attrs.class;
+				var newclass=oldclass.replace("span3","span6");
+				attrs.$set('class',newclass);
+			});
+			
+		},
+		template: '<div ng-init="a=0;" ng-click="a=!a"><h1>{{subject}}</h1><span class="detail" ng-show="a" ng-bind-html-unsafe="detail"></span></div>',
+		replace:true
 	}
 });
 app.directive('expand', function () {
@@ -13,12 +26,13 @@ app.directive('expand', function () {
 	return {
 		restrict: 'A',
 		link: function (scope, elem, attrs) {
+		/*
 			elem.on('click',function(){
 				var oldclass=attrs.class;
 				var newclass=oldclass.replace("span3","span6");
 				attrs.$set('class',newclass);
 			});
-			
+		*/
 		}
 	}
 });
