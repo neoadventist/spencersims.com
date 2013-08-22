9137a86339c9bbd28ccd5f997cefd2766ed37622
Spencer Sims <me@spencersims.com>
2013-08-22 02:56:13 -0400

    added cv, projects files and routes

--- assets/model/app.js
+++ assets/model/app.js
@@ -1,9 +1,8 @@
 var app = angular.module('SPENCERSIMS', ['ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/main', {templateUrl: 'assets/view/main.html',   controller: 'main'}).
-	  when('/download', {templateUrl: 'assets/view/download.html',   controller: 'download'}).
-	  otherwise({redirectTo: '/main'});
+    when('/main', {templateUrl: 'assets/view/main.html',controller:'main'}).
+    when('/cv', {templateUrl: 'assets/view/cv.html',controller:'cv'}).
+    when('/projects',{templateUrl: 'assets/view/projects.html',controller:'projects'}).
+    otherwise({redirectTo: '/main'});
 }]);
-
-
